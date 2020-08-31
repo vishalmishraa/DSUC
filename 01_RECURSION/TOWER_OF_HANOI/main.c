f8dd35d4a3c5122014247c40d2db66aec756dff8
vishalmisharaa <vishal.mishra9470@gmail.com>
2020-08-31 12:14:23 +0530

    towe_of_hanoi

---
+++ 01_RECURSION/TOWER_OF_HANOI/main.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+//n = number of disk
+//a = from 
+//b = intermediate/using
+//c = destination
+static int i=1;
+int TOH(int n,int a,int b,int c){
+    if(n>0){
+        TOH(n-1,a,c,b);
+        printf("\nstep %d :(%d to %d)\n",i++,a,c);
+        TOH(n-1,b,a,c);
+    }
+};
+
+int main(){
+    TOH(3,1,2,3);
+    return 0;
+}
